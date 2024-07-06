6cc80612706b7c02e03afa3970433984ef778e94
Vydeekelz <Provydee95@gmail.com>
2024-07-06 01:59:07 +0100

    sample codes

--- app.js
+++ app.js
@@ -1,1 +1,16 @@
-console.log('Task manager app'); 
+const express = require('express');
+const path = require('path');
+const app = express();
+
+port = 5000
+
+app.get('/', (req, res) => {
+    console.log('user hit the resource')
+    res.status(200).sendFile(path.resolve(__dirname, './public/index.html'))
+})
+
+app.listen(port, (req, res) => {
+    console.log(`App is listening on port ${port}`)
+ 
+
+})
